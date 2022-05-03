a43ac522905127ab4ba4a59f6da97c7a87d3cfb4
Dading Chen <dading.chen@stonybrook.edu>
2022-05-03 17:28:14 -0400

    one of header that include a method can give a brief intro about the game

---
+++ introduction.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef INTRODUCTION_H
+#define INTRODUCTION_H
+class introduction
+{	//print out intro
+	public:
+	void intro(){
+	
+	std::cout<<"Welcome to wordle game!"<<std::endl;
+	std::cout<<"You have 6 tries for guessing a five letters word. Now you may begin!"<<std::endl;
+	
+	}
+
+
+
+
+
+
+};
+#endif
